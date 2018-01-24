1ddcf458db4e81b867d34f1ef77443a470a72460
Harleigh Abel <harleighabel@gmail.com>
2018-01-24 18:44:23 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,20 +1,15 @@
 def valid_move?(board, index)
-  if index.between?(0,8)
-    move = position_taken?(board, index)
-    if move == false
+  if index.between?(0,8) && position_taken?(board, index) == falsee
       true
     else
       false
-    end
-  else
-    false
   end
+endS      
 
-  def position_taken?(board, index)
-   if board[index] == " " ||board[index] == "" || board[index] == nil
-     return false
-   else
-     true
-   end
+def position_taken?(board, index)
+  if board[index] == " " ||board[index] == "" || board[index] == nil
+    return false
+  else
+    true
   end
 end
