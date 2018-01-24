199279c458cb3fbbcef72ef52a4b5ef3ceffd6d6
Harleigh Abel <harleighabel@gmail.com>
2018-01-24 18:29:33 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,14 +7,5 @@ def valid_move?(index, board)
     end
   else
     false
-  end        
-end
-
-
-  move = board[index]
-  if move.between?(0,8) && position_taken?(move) == false
-    return true
-  else
-    false
   end
 end
